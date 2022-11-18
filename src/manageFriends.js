8ef6a296f811cbed570610d50307ad2bb226fb68
Rebecca Sissman <rsissman15@aol.com>
2022-11-18 11:34:31 -0500

    Completed lab

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,10 @@
-export function manageFriends(state, action) {
-  // your code here
+export function manageFriends(state = { friends: [] }, action) {
+  switch (action.type) {
+    case 'friends/add':
+      return { friends: [...state.friends, action.payload] };
+    case 'friends/remove':
+      return { friends: []};
+    default:
+      return state;
+  }
 }
